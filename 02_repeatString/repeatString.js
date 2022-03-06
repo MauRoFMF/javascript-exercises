b25174adc8a4c6923a775834d96328b89b5d66e3
Mauro Ferreira <mauro.ferreira1996@gmail.com>
2022-03-06 10:19:54 +0100

    Solved repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num === 0) {
+    return "";
+  } else if (num < 0) {
+    return "ERROR";
+  }
+  let remainString = string;
+  for (let i = 1; i < num; i++) {
+    remainString = string + remainString;
+  }
+  return remainString;
 };
 
 // Do not edit below this line
